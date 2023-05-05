9186d997160a09b8cd3efb622d191b5bdb6694c7
RomanKozlovskiy <roma.kozlovskiy.96@mail.ru>
2023-05-05 23:46:45 +0300

    first commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Роман Козловский on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
