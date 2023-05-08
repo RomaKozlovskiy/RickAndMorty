7d760622f29fd0c1cb6b3dff6887c82ac460c832
RomanKozlovskiy <roma.kozlovskiy.96@mail.ru>
2023-05-08 13:32:04 +0300

    Work in CharactersTableVC - setup searchController, search logic, navigationBar.

--- RickAndMortyApp/ViewControllers/CharactersTableViewController.swift
+++ RickAndMortyApp/ViewControllers/CharactersTableViewController.swift
@@ -8,24 +8,91 @@
 import UIKit
 
 class CharactersTableViewController: UITableViewController {
-
+    
+    // MARK: - Private Properties
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var rickAndMorty: RickAndMorty?
+    private var filteredCharacters: [Result] = []
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        searchController.isActive && !searchBarIsEmpty
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupSearchController()
+        setupNavigationBar()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        isFiltering
+        ? filteredCharacters.count
+        : rickAndMorty?.results.count ?? 0
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-
+        let character = isFiltering
+        ? filteredCharacters[indexPath.row]
+        : rickAndMorty?.results[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
+        
+        cell.nameLabel.text = character?.name
+        
         return cell
     }
     
+    // MARK: - Private Methods
+    
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        searchController.searchBar.tintColor = .white
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
+        if let textField = searchController.searchBar.value(forKey: "searchField") as? UITextField {
+            textField.font = .systemFont(ofSize: 17)
+            textField.textColor = .white
+        }
+    }
+    
+    private func setupNavigationBar() {
+        title = "Rick & Morty"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        if #available(iOS 13.0, *) {
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithOpaqueBackground()
+            navBarAppearance.backgroundColor = .black
+            navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+            
+            navigationController?.navigationBar.standardAppearance = navBarAppearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        }
+    }
+}
+
+// MARK: - UISearchResultsUpdating
+extension CharactersTableViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        filterContentForSearchTExt(searchController.searchBar.text!)
+    }
+    
+    private func filterContentForSearchTExt(_ searchText: String) {
+        filteredCharacters = rickAndMorty?.results.filter({ character in
+            character.name.lowercased().contains(searchText.lowercased())
+        }) ?? []
+    }
 }
