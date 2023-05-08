4f38d34c7b6c86be53b36ee3f8b67e9ab97ade4a
RomanKozlovskiy <roma.kozlovskiy.96@mail.ru>
2023-05-08 14:16:47 +0300

    work with NetworkManager

--- RickAndMortyApp/Services/NetworkManager.swift
+++ RickAndMortyApp/Services/NetworkManager.swift
@@ -6,3 +6,74 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
+    
+    // MARK: - fetchData
+    func fetchData(from url: String?, completion: @escaping(RickAndMorty) -> Void) {
+        guard let stringUrl = url else { return }
+        guard let url = URL(string: stringUrl) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                print("Some error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let characters = try JSONDecoder().decode(RickAndMorty.self, from: data)
+                DispatchQueue.main.async {
+                    completion(characters)
+                }
+            } catch let jsonError {
+                print("Failed to decode JSON!", jsonError)
+            }
+        }.resume()
+    }
+    
+    // MARK: - fetchEpisode
+    private func fetchEpisode(from url: String, completion: @escaping (Episode) -> Void) {
+        guard let url = URL(string: url) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print("Some error: \(error?.localizedDescription ?? "No description")")
+                return
+            }
+            
+            do {
+                let episode = try JSONDecoder().decode(Episode.self, from: data)
+                DispatchQueue.main.async {
+                    completion(episode)
+                }
+            } catch let jsonError {
+                print("Failed to decode JSON!", jsonError)
+            }
+        }.resume()
+    }
+    
+    // MARK: - fetchCharacter
+    private func fetchCharacter(from url: String, completion: @escaping (Result) -> Void) {
+        guard let url = URL(string: url) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print("Some error: \(error?.localizedDescription ?? "No description")")
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(Result.self, from: data)
+                DispatchQueue.main.async {
+                    completion(result)
+                }
+            } catch let jsonError {
+                print("Failed to decode JSON!", jsonError)
+            }
+        }.resume()
+    }
+}
